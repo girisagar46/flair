0d9f1199fef9909d40e7343bd756729983aba0ab
Sagar Giri <sagar.giri@octoenergy.com>
2023-06-18 23:02:05 +0900

    Make it better

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
+import csv
+import os
+
 from flask import Flask
 from flask import render_template
 
-from settings import APP_STATIC, DATA_STATIC
-import csv, os
-
-from services import ReportFetchService as RFS
+from services.ReportFetchService import fetch_files
+from settings import DATA_STATIC
 
 app = Flask(__name__)
 static_dir = os.path
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    with open(os.path.join(DATA_STATIC, 'fyp.csv'), 'rb') as file:
+    with open(os.path.join(DATA_STATIC, "fyp.csv")) as file:
         dList = []
         reader = csv.DictReader(file)
         for row in reader:
             dList.append(row)
-    return render_template("index.html", data = dList)
+    return render_template("index.html", data=dList)
 
 
-@app.route('/view/<roll>')
+@app.route("/view/<roll>")
 def view_content(roll):
-    report_data = RFS.fetch_files(roll.split('&'))
-    return render_template('details.html', data=report_data)
+    report_data = fetch_files(roll.split("&"))
+    return render_template("details.html", data=report_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- services/ReportFetchService.py
+++ services/ReportFetchService.py
@@ -1,18 +1,17 @@
 import requests
-import os
+
+
 def fetch_files(roll):
     r = requests.get("https://api.github.com/repos/CSIT-GUIDE/FYP-2016/contents")
     payload_json = r.json()
-    name, htmlurl,download_url = ("", "", "")
+    name, html_url, download_url = None, None, None
     for each in payload_json:
-        temp_name = each.get('name').split('_')
+        temp_name = each.get("name").split("_")
         if roll[0] in temp_name:
-            name = each.get('name')
-            htmlurl = each.get('html_url')
-            download_url = each.get('download_url')
+            name = each.get("name")
+            html_url = each.get("html_url")
+            download_url = each.get("download_url")
         else:
             pass
-    return [name, htmlurl, download_url]
+    return name, html_url, download_url
 
-if __name__ == "__main__":
-    fetch_files()
